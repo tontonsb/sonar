066dec1ade051feccb41bfbb67d610c6d525c6b2
Džuris <juris@glaive.pro>
2024-04-19 20:24:09 +0300

    Experiment

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ import 'ol/ol.css'
 import Map from 'ol/Map.js'
 import OSM from 'ol/source/OSM.js'
 import TileLayer from 'ol/layer/Tile.js'
+import VectorLayer from 'ol/layer/Vector.js'
 import View from 'ol/View.js'
+import VectorSource from 'ol/source/Vector'
+import KML from 'ol/format/KML'
+import { fromLonLat } from 'ol/proj'
 
 const map = new Map({
   target: 'map',
@@ -11,9 +15,15 @@ const map = new Map({
     new TileLayer({
       source: new OSM(),
     }),
+	new VectorLayer({
+		source: new VectorSource({
+			url: 'samples/doc_web.kml',
+			format: new KML(),
+		}),
+	}),
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2,
+    center: fromLonLat([23.54, 56.87]),
+    zoom: 12,
   }),
 })
