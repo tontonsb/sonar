c4fdc323099514f62f42f12285199e14987b30d9
Džuris <juris@glaive.pro>
2024-05-03 19:47:19 +0300

    Make arg parsing safer

--- prepkml.php
+++ prepkml.php
@@ -6,6 +6,6 @@ require __DIR__.'/vendor/autoload.php';
 
 Config::set('base', 'https://sonar.glaive.pro/kml');
 Config::set('placeholder', 'URLTOKEN');
-Config::set('location', $argv[1]);
+Config::set('location', $argv[1] ?? null);
 
 Processor::process();
